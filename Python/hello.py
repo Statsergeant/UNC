1e6ffdf76583901b000748ac4188c434726b923c
Statsergeant <110009528+Statsergeant@users.noreply.github.com>
2025-02-24 08:28:58 -0500

    Update hello.py

--- Python/hello.py
+++ Python/hello.py
@@ -8,4 +8,4 @@ def home():
     return render_template('user.html')  # Passing a list
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', debug=True)
+    app.run(host = '0.0.0.0', port = 5000, debug=True)
