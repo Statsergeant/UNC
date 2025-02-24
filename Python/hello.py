d5064868c081dc6a1c89a86cf8da28ba74f19ba2
Statsergeant <110009528+Statsergeant@users.noreply.github.com>
2025-02-24 08:19:35 -0500

    Add files via upload

---
+++ Python/hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('user.html')  # Passing a list
+
+if __name__ == '__main__':
+    app.run(debug=True)
